5a427b9e050fbbc626bec2a787ce0be4ad6ee0b8
venkatasai-ptl <venkatasai.ptl@gmail.com>
2024-07-10 10:21:03 -0500

    modified:   requirements.txt 	modified:   setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="babloo",
+    author_email="bhuvaneshaashi@gmail.com",
+    packages=find_packages()
+)
